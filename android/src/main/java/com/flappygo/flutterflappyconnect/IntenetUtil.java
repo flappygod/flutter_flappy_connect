1929e1d057da6485643f38dda0c7f881a7e04a8f
lijunlin <327603258@qq.com>
2022-01-14 11:06:26 +0800

    flappy connect

--- android/src/main/java/com/flappygo/flutterflappyconnect/IntenetUtil.java
+++ android/src/main/java/com/flappygo/flutterflappyconnect/IntenetUtil.java
@@ -6,35 +6,33 @@ import android.net.NetworkInfo;
 import android.telephony.TelephonyManager;
 
 public class IntenetUtil {
-    //没有网络连接
+    //no network
     public static final int NETWORN_NONE = 0;
-    //wifi连接
+    //is wifi
     public static final int NETWORN_WIFI = 1;
     public static final int UnCon_WIFI = 7;
-    //手机网络数据连接类型
+    //mobile
     public static final int NETWORN_2G = 2;
     public static final int NETWORN_3G = 3;
     public static final int NETWORN_4G = 4;
     public static final int NETWORN_5G = 5;
     public static final int NETWORN_MOBILE = 6;
-    public static final int NETWORN_ETHERNET=7;
-    /**
-     * 获取当前网络连接类型
-     * @param context
-     * @return
-     */
+    public static final int NETWORN_ETHERNET = 7;
+
+    //get network state
     public static int getNetworkState(Context context) {
-        //获取系统的网络服务
+        //get mananger
         ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        //如果当前没有网络
-        if (null == connManager)
+        //none
+        if (null == connManager) {
             return NETWORN_NONE;
-        //获取当前网络类型，如果为空，返回无网络
+        }
+        //get type
         NetworkInfo activeNetInfo = connManager.getActiveNetworkInfo();
         if (activeNetInfo == null || !activeNetInfo.isAvailable()) {
             return NETWORN_NONE;
         }
-        // 判断是不是连接的是不是wifi
+        // check is wifi or not
         NetworkInfo wifiInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
         if (null != wifiInfo) {
             NetworkInfo.State state = wifiInfo.getState();
@@ -42,11 +40,11 @@ public class IntenetUtil {
                 if (state == NetworkInfo.State.CONNECTED
                         || state == NetworkInfo.State.CONNECTING) {
                     return NETWORN_WIFI;
-                }else if(state == NetworkInfo.State.DISCONNECTED){
+                } else if (state == NetworkInfo.State.DISCONNECTED) {
                     return UnCon_WIFI;
                 }
         }
-        // 如果不是wifi，则判断当前连接的是运营商的哪种网络2g、3g、4g等
+        // check G
         NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
         if (null != networkInfo) {
             NetworkInfo.State state = networkInfo.getState();
@@ -55,16 +53,16 @@ public class IntenetUtil {
                 if (state == NetworkInfo.State.CONNECTED
                         || state == NetworkInfo.State.CONNECTING) {
                     switch (activeNetInfo.getSubtype()) {
-                            // 联通2g
+                        // liantong 2g
                         case TelephonyManager.NETWORK_TYPE_GPRS:
-                            // 电信2g
+                            // dianxin 2g
                         case TelephonyManager.NETWORK_TYPE_CDMA:
                         case TelephonyManager.NETWORK_TYPE_EDGE:
-                            // 移动2g
+                            // yidong 2g
                         case TelephonyManager.NETWORK_TYPE_1xRTT:
                         case TelephonyManager.NETWORK_TYPE_IDEN:
                             return NETWORN_2G;
-                            //如果是3g类型
+                        //3g
                         case TelephonyManager.NETWORK_TYPE_EVDO_A:
                         case TelephonyManager.NETWORK_TYPE_UMTS:
                         case TelephonyManager.NETWORK_TYPE_EVDO_0:
@@ -75,13 +73,14 @@ public class IntenetUtil {
                         case TelephonyManager.NETWORK_TYPE_EHRPD:
                         case TelephonyManager.NETWORK_TYPE_HSPAP:
                             return NETWORN_3G;
-                        //如果是4g类型
+                        //4g
                         case TelephonyManager.NETWORK_TYPE_LTE:
                             return NETWORN_4G;
+                        //5g
                         case TelephonyManager.NETWORK_TYPE_NR:
                             return NETWORN_5G;
                         default:
-                            //中国移动 联通 电信 三种3G制式
+                            //3G
                             if (strSubTypeName.equalsIgnoreCase("TD-SCDMA")
                                     || strSubTypeName.equalsIgnoreCase("WCDMA")
                                     || strSubTypeName.equalsIgnoreCase("CDMA2000")) {
@@ -93,7 +92,7 @@ public class IntenetUtil {
                 }
         }
         NetworkInfo EthernetInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_ETHERNET);
-        if (null != EthernetInfo){
+        if (null != EthernetInfo) {
             NetworkInfo.State state = EthernetInfo.getState();
             if (null != state)
                 if (state == NetworkInfo.State.CONNECTED || state == NetworkInfo.State.CONNECTING) {
